99b222f62e315ab137da703a6c29c42f827642c8
Roy Mor <roy.mor173@gmail.com>
2017-02-20 17:10:17 +0200

    feat(toast): add shifting and close button for toasts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { create, kill } from './toast';
+import { create, kill, shift } from './toast';
 
 const install = function (Vue, options = {maxToasts: 6}) {
   if (this.installed) {
@@ -9,9 +9,12 @@ const install = function (Vue, options = {maxToasts: 6}) {
   Vue.maxToasts = options.maxToasts;
   Vue.prototype.$toast = function (options) {
     if (Vue.toasts.length >= Vue.maxToasts) {
-      kill(Vue.toasts, Vue.toasts.length - 1, options);
+      kill(Vue.toasts, 0, options);
     }
     create(Vue.toasts, options);
+    if (Vue.toasts.length > 1) {
+      shift(Vue.toasts, options);
+    }
   };
 };
 
--- src/toast.js
+++ src/toast.js
@@ -2,17 +2,38 @@ import $ from "jquery";
 import style from './css/toast.scss';
 
 const TOAST_CLASS = style.toast;
+const TOAST_CLOSE_BUTTON_CLASS = style['toast-close-button'];
 const TOAST_DEFAULT_BORDER_RADIUS = 0;
 const TOAST_DEFAULT_COLOR = 'white';
 const TOAST_DEFAULT_BACKGROUND_COLOR = 'rgba(60,118,61,.9)';
 const TOAST_DEFAULT_POSITION = 'bottom right';
 const TOAST_DEFAULT_FADE_SPEED = 400;
 const TOAST_DEFAULT_TIME = 3000;
+const TOAST_MARGIN = 5;
 const POSITION_FN = {
   'bottom left': positionToastBottomLeft,
   'bottom right': positionToastBottomRight,
   'top left': positionToastTopLeft,
   'top right': positionToastTopRight
+};
+const SHIFT_FN = {
+  'bottom left': shiftUp,
+  'bottom right': shiftUp,
+  'top left': shiftDown,
+  'top right': shiftDown
+};
+
+const kill = (toasts, index, options) => {
+  const $toast = toasts[index];
+  if ($toast.$closeButton) {
+    $toast.$closeButton.off('click');
+  }
+  $toast
+    .stop()
+    .fadeOut(options.fade || TOAST_DEFAULT_FADE_SPEED, () => {
+      $toast.detach();
+    });
+  toasts.splice(index, 1);
 };
 
 const create = (toasts, options) => {
@@ -20,36 +41,73 @@ const create = (toasts, options) => {
   const $toast = $(document.createElement('div'));
   $toast.html(options.message);
   $toast.css({
-    borderRadius: TOAST_DEFAULT_BORDER_RADIUS,
+    borderRadius: options.borderRadius || TOAST_DEFAULT_BORDER_RADIUS,
     color: options.color || TOAST_DEFAULT_COLOR,
     backgroundColor: options.backgroundColor || TOAST_DEFAULT_BACKGROUND_COLOR
   });
+  if (options.fixedWidth) {
+    $toast.css({
+      width: options.fixedWidth,
+      textOverflow: 'ellipsis'
+    });
+  }
   $toast.addClass(TOAST_CLASS);
   POSITION_FN[options.position || TOAST_DEFAULT_POSITION]($toast);
-  const toastIndex = toasts.length;
   $toast
     .hide()
     .appendTo($body)
     .fadeIn(options.fade || TOAST_DEFAULT_FADE_SPEED);
-  setTimeout(() => {
-    $toast
-      .fadeOut(options.fade || TOAST_DEFAULT_FADE_SPEED, () => {
-        $toast.detach();
-        toasts.splice(toastIndex, 1);
-      });
-  }, options.time || TOAST_DEFAULT_TIME);
+  $toast.toastIndex = toasts.length;
+  if (options.closeButton) {
+    const $closeButton = createCloseButton(options);
+    $toast.$closeButton = $closeButton;
+    const toastHeight = $toast.outerHeight();
+    $closeButton.click(() => {
+      kill(toasts, $toast.toastIndex, options);
+      reverseShift(toasts, $toast.toastIndex, toastHeight, options);
+    });
+    $toast.append($closeButton);
+  } else {
+    setTimeout(() => {
+      $toast
+        .fadeOut(options.fade || TOAST_DEFAULT_FADE_SPEED, () => {
+          $toast.detach();
+          toasts.shift();
+        });
+    }, options.time || TOAST_DEFAULT_TIME);
+  }
   toasts.push($toast);
 };
 
-const kill = (toasts, index, options) => {
-  const $toast = toasts[index];
-  $toast
-    .stop()
-    .fadeOut(options.fade || TOAST_DEFAULT_FADE_SPEED, () => {
-      $toast.detach();
-      toasts.splice(index, 1);
-    });
+const shift = (toasts, options) => {
+  const latestToastHeight = toasts[toasts.length - 1].outerHeight();
+  const shiftFn = SHIFT_FN[options.position || TOAST_DEFAULT_POSITION];
+  for (let i = 0; i < toasts.length - 1; i++) {
+    const $toast = toasts[i];
+    shiftFn($toast, latestToastHeight + TOAST_MARGIN);
+  }
 };
+
+function createCloseButton(options) {
+  const $closeButton = $(document.createElement('span'));
+  $closeButton.css({
+    color: options.color || TOAST_DEFAULT_COLOR
+  });
+  $closeButton.addClass(TOAST_CLOSE_BUTTON_CLASS);
+  return $closeButton;
+}
+
+function reverseShift(toasts, closedToastIndex, closedToastHeight, options) {
+  const shiftFn = SHIFT_FN[options.position || TOAST_DEFAULT_POSITION];
+  for (let i = closedToastIndex; i < toasts.length; i++) {
+    const $toast = toasts[i];
+    $toast.toastIndex--;
+  }
+  for (let i = closedToastIndex - 1; i >= 0; i--) {
+    const $toast = toasts[i];
+    shiftFn($toast, -(closedToastHeight + TOAST_MARGIN));
+  }
+}
 
 function positionToastBottomLeft($toast) {
   $toast.css({
@@ -79,4 +137,16 @@ function positionToastTopRight($toast) {
   });
 }
 
-export { create, kill };
+function shiftUp($toast, shiftBy) {
+  $toast.animate({
+    bottom: `+=${shiftBy}px`
+  });
+}
+
+function shiftDown($toast, shiftBy) {
+  $toast.animate({
+    top: `+=${shiftBy}px`
+  });
+}
+
+export { create, kill, shift };
