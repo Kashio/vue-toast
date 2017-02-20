154c6b2cf7c43d2cad170bb963b872c4a0a2ce81
Roy Mor <roy.mor173@gmail.com>
2017-02-20 17:10:56 +0200

    feat(toast): add random message for the creation of the toasts for testing purposes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,18 @@ import { VueToast } from './index.js';
 
 Vue.use(VueToast);
 
+function randomMessage() {
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const numberOfCharacters = Math.floor(Math.random() * 100);
+  for (let i = 0; i < numberOfCharacters; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+Vue.maxToasts = 10;
+
 const vm = new Vue({
   el: '#app',
   render: h => h('button', {
@@ -16,7 +28,13 @@ const vm = new Vue({
     on: {
       click() {
         vm.$toast({
-          message: 'Test Toast'
+          message: randomMessage(),
+          borderRadius: 7,
+          color: 'black',
+          backgroundColor: '#e2eef9',
+          fixedWidth: 300,
+          closeButton: true,
+          time: 5000
         });
       }
     }
